76e769195385b4edacda2fa9c0474ec17a84c22b
Tanucode <96375246+Tanucode@users.noreply.github.com>
2022-01-17 23:49:03 +0530

    Delete leap_year.py

--- leap_year.py
+++
@@ -1,7 +0,0 @@
-def check_leap(year):
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        return ("This is a leap year!!")
-    else:
-        return("This is not a leap year!!")
-
-
