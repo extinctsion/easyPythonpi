7194e4fa753a49b624eb8623f531a64e404121c6
AKG1301 <abhinavkgupta2001@gmail.com>
2022-10-12 21:00:43 +0530

    add count vowels function

--- easyPythonpi/easyPythonpi.py
+++ easyPythonpi/easyPythonpi.py
@@ -296,9 +296,30 @@ def matrix_transpose(matrix1):
                                             
 def remove_punctuation(my_str):
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    # remove punctuation from the string
+    # remove punctuations from the string
     no_punct = ""
+    #run a for loop and traverse every element in a string and check ,if char is not match with punctuations char then it will add in no_punct
     for char in my_str:
         if char not in punctuations:
             no_punct = no_punct + char
+    #return no_punct
+    return no_punct
+
+def count_vowels(ip_str):
+    # string of vowels
+    vowels = 'aeiou'
+    # make it suitable for comparisions
+    ip_str = ip_str.casefold()
+
+    # make a dictionary with each vowel a key and value 0
+    count = {}.fromkeys(vowels,0)
+
+    # count the vowels
+    for char in ip_str:
+        if char in count:
+            count[char] += 1
+            
+    #return the count dictionary
+    return count
+
 
