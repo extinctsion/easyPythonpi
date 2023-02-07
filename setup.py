e545718d49546325eef860799d3b1e8f589a9234
Aditya Sharma <67048929+extinctsion@users.noreply.github.com>
2023-02-08 00:00:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
 from setuptools import setup, find_packages
+import subprocess
 import codecs
 import os
 
-VERSION = '0.0.1'
+#VERSION = '0.0.1'
+VERSION = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+if "-" in VERSION:
+    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
+    # pip has gotten strict with version numbers
+    # so change it to: "1.3.3+22.git.gdf81228"
+    # See: https://peps.python.org/pep-0440/#local-version-segments
+    v,i,s = VERSION.split("-")
+    VERSION = v + "+" + i + ".git." + s
+
+# assert "-" not in cf_remote_version
+# assert "." in cf_remote_version
+
+# assert os.path.isfile("cf_remote/version.py")
+# with open("cf_remote/VERSION", "w", encoding="utf-8") as fh:
+#     fh.write("%s\n" % cf_remote_version)
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
 DESCRIPTION = 'A simple library for Python beginners'
 
 # Setting up
