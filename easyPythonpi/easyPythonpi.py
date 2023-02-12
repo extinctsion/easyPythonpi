5a8670a67c6c336f29950044c80f033828c2c019
AndrewHUNGNguyen <42189273+AndrewHUNGNguyen@users.noreply.github.com>
2023-02-12 06:54:18 -0800

    hacktoberfest contribution - binary to hexadecimal (#31)
    * hacktoberfest contribution - binary to hexadecimal
    
    * Adding regexp to detect invalid expressions and unit test cases

--- easyPythonpi/easyPythonpi.py
+++ easyPythonpi/easyPythonpi.py
@@ -1,3 +1,4 @@
+import regex as re
 """  A python module that helps you to calculate some of the  most used calculations.....
     usage--
             Just download the file from git and unzip in ur system.
@@ -128,7 +129,87 @@ def bin2dec(x:'bin')->'dec':
         
             a=a+1
     return r                  
-             
+
+# A function that converts a binary string to hexadecimal
+def bin2hex(x:'bin')->'hex': 
+    """Converts a binary string to a hexadecimal string.
+
+    This function converts a binary string to hexadecimal. If the binary
+    string's length is a multiple of 4, simply break up the string into 
+    substrings of 4 binary digits and determine their hexadecimal digit.
+    If the binary string's length not a multiple of 4 prepend the 
+    necessary number of 0's to make the string a multiple of 4.
+
+    Args:
+        x ('bin') : Binary string to convert to hexadecimal.
+
+    Returns:
+        The binary converted to hexadecimal.
+
+    Raises:
+        No errors 
+    """
+
+    h = ''  # hexadecimal number converted from binary and to be returned
+
+    
+    x=str(x)
+    
+    # Determine if the string has invalid characters
+    if re.search('[^(0-1)]', x):
+        raise InvalidBinaryException 
+
+    # Get the length of the string
+    l=len(x)
+
+    # Begin the process of converting x to its hexadecimal number
+
+    # If the length is not a multiple of 4, prepend 0's before converting
+    if l % 4 != 0:
+        numZerosPrepended = 4 - ( l % 4 ) # number of zeros to prepend 
+        x = (numZerosPrepended * '0') + x # concatenate numZerosPrepended to x
+
+    
+    for i in range(len(x)-1, 0, -4):
+        substring = x[i-3:i+1]    # The substring converted to a hex character
+
+        # Determine the binary substring's hex and prepend to h
+        if substring == '0000':
+            h = '0' + h
+        elif substring == '0001':
+            h = '1' + h
+        elif substring == '0010':
+            h = '2' + h
+        elif substring == '0011':
+            h = '3' + h            
+        elif substring == '0100':
+            h = '4' + h
+        elif substring == '0101':
+            h = '5' + h
+        elif substring == '0110':
+            h = '6' + h
+        elif substring == '0111':
+            h = '7' + h
+        elif substring == '1000':
+            h = '8' + h
+        elif substring == '1001':
+            h = '9' + h
+        elif substring == '1010':
+            h = 'A' + h
+        elif substring == '1011':
+            h = 'B' + h
+        elif substring == '1100':
+            h = 'C' + h
+        elif substring == '1100':
+            h = 'C' + h
+        elif substring == '1101':
+            h = 'D' + h
+        elif substring == '1110':
+            h = 'E' + h
+        elif substring == '1111':
+            h = 'F' + h
+
+    return h        
 
 def createarray(length:'int',dtype='int')->'array':   # To create an array of entered length and entered data type(interger data type is a default data type)
     import numpy as np 
@@ -333,3 +414,9 @@ def count_vowels(ip_str:'str')->'int':
     #return the count dictionary
     return count
 
+
+# Programmer defined exceptions go here:
+
+# define exception for invalid Binary Strings
+class InvalidBinaryException(Exception):
+    pass
