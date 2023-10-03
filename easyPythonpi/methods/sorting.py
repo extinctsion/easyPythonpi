93bab0bbf31f2d18d4498ee20ffeadf727d08a14
Jugal Kishore Das <94907322+Jugalcody@users.noreply.github.com>
2023-10-03 23:05:55 +0530

    structure modified (#49)
    * modified
    
    * modified struct

---
+++ easyPythonpi/methods/sorting.py
@@ -0,0 +1,9 @@
+
+def sort(list:'list'):    # To bubble sort and array or list
+    for i in range(len(list) - 1, 0, -1):
+        for j in range(i):
+            if list[j] > list[j + 1]:
+                temp = list[j]
+                list[j] = list[j + 1]
+                list[j + 1] = temp
+
